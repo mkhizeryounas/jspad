6a943b1f4a7de890fac2b979597a1e06a5e454ae
Khizer Younas <m.khizeryounas@gmail.com>
2021-03-16 04:40:22 +0500

    feat: edit username added

--- src/components/Console/index.jsx
+++ src/components/Console/index.jsx
@@ -8,7 +8,7 @@ const Component = (props) => {
   return (
     <div className={`console col ${className}`}>
       <tt>
-        <h5>Activity Panal</h5>
+        <h5>Activity Panel</h5>
         <hr />
         <ActiveUsers />
 
