aa1f05cbd2b3b6faa7733e3533a301867c7c32d1
Khizer Younas <m.khizeryounas@gmail.com>
2021-03-15 17:47:53 +0500

    feat: initial commit

--- src/components/ActiveUsers/index.jsx
+++ src/components/ActiveUsers/index.jsx
@@ -20,19 +20,23 @@ const Component = (props) => {
     <div className='container mb-2'>
       <strong className='mb-2 row '>Active Users</strong>
 
-      {Object.entries(users).map(([key, value], index) => {
-        return (
-          <div key={`${index}-${key}`} className='row mb-1'>
-            <i
-              className='fa fa-user-circle mr-1'
-              style={{
-                color: value.color,
-              }}
-            ></i>{' '}
-            {key}
-          </div>
-        );
-      })}
+      {users ? (
+        Object.entries(users).map(([key, value], index) => {
+          return (
+            <div key={`${index}-${key}`} className='row mb-1'>
+              <i
+                className='fa fa-user-circle mr-1'
+                style={{
+                  color: value.color,
+                }}
+              ></i>{' '}
+              {key}
+            </div>
+          );
+        })
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
