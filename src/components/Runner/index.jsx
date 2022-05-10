b7590f940a7cf3095f3ba8b1b02f02d6603737bb
Khizer Younas <m.khizeryounas@gmail.com>
2022-05-10 13:21:19 -0700

    feat: real compiler added

--- src/components/Runner/index.jsx
+++ src/components/Runner/index.jsx
@@ -1,55 +1,87 @@
 import { useState } from 'react';
 import './style.css';
 import copy from 'copy-to-clipboard';
-import { useToasts } from 'react-toast-notifications';
-import { PUBLIC_URL } from '../../utils/config';
+// import { useToasts } from 'react-toast-notifications';
+// import { PUBLIC_URL } from '../../utils/config';
+import axios from 'axios';
 
 const Component = (props) => {
-  const { script = '', language = 'nodejs' } = props;
+  const { script = '', language = 'javascript' } = props;
   const [isLoading, setIsLoading] = useState(false);
-  const { addToast } = useToasts();
+  // const { addToast } = useToasts();
 
   const callCallbak = (res) => {
-    props.onOutput && props.onOutput(res);
     setIsLoading(false);
+    props.onOutput && props.onOutput(res);
   };
+
+  // const executeCode = async ({ script, language }) => {
+  //   setIsLoading(true);
+  //   props.onOutput && props.onOutput(null);
+
+  //   const worker = new Worker(PUBLIC_URL + '/worker.js');
+
+  //   setTimeout(() => {
+  //     let res = null;
+  //     worker.postMessage(script);
+
+  //     // handle output
+  //     worker.addEventListener('message', function (e) {
+  //       console.log('Message from Worker: ', e.data);
+  //       res = e.data;
+  //       callCallbak(res);
+  //     });
+
+  //     // handle timeouts
+  //     setTimeout(() => {
+  //       worker.terminate();
+  //       if (!res) {
+  //         const timeoutMsg = 'Timeout reached before any output';
+  //         res = {
+  //           date: new Date().toString(),
+  //           hasError: true,
+  //           stdout: [timeoutMsg],
+  //         };
+
+  //         addToast(timeoutMsg, {
+  //           appearance: 'error',
+  //         });
+  //         callCallbak(res);
+  //       }
+  //     }, 5 * 1000);
+  //   }, 500);
+  // };
 
   const executeCode = async ({ script, language }) => {
-    setIsLoading(true);
-    props.onOutput && props.onOutput(null);
-
-    const worker = new Worker(PUBLIC_URL + '/worker.js');
-
-    setTimeout(() => {
-      let res = null;
-      worker.postMessage(script);
-
-      // handle output
-      worker.addEventListener('message', function (e) {
-        console.log('Message from Worker: ', e.data);
-        res = e.data;
-        callCallbak(res);
-      });
-
-      // handle timeouts
-      setTimeout(() => {
-        worker.terminate();
-        if (!res) {
-          const timeoutMsg = 'Timeout reached before any output';
-          res = {
-            date: new Date().toString(),
-            hasError: true,
-            stdout: [timeoutMsg],
-          };
-
-          addToast(timeoutMsg, {
-            appearance: 'error',
-          });
-          callCallbak(res);
+    let res = {
+      date: new Date().toString(),
+    };
+    try {
+      console.log('Executing code...', language, script);
+      setIsLoading(true);
+      props.onOutput && props.onOutput(null);
+      const execResponse = await axios.post(
+        `https://cors-anywhere.herokuapp.com/https://sharepad.io/run`,
+        { lang: language, code: script, stdin: '' },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Requested-With': 'XMLHttpRequest',
+          },
         }
-      }, 5 * 1000);
-    }, 500);
+      );
+
+      res['hasError'] = !execResponse.data.success;
+      res['stdout'] = [execResponse.data.stdout];
+
+      console.log('Executed code...', execResponse);
+    } catch (err) {
+      res['hasError'] = false;
+      res['stdout'] = [err.response.data];
+    }
+    callCallbak(res);
   };
+
   return (
     <div className='row runner'>
       <div className='col'>
