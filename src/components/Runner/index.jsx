9610086add3c6b87d3514598292f6c73e481d768
Khizer Younas <m.khizeryounas@gmail.com>
2022-05-12 17:27:14 -0700

    fix: removed compiler

--- src/components/Runner/index.jsx
+++ src/components/Runner/index.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import './style.css';
 import copy from 'copy-to-clipboard';
 import { useToasts } from 'react-toast-notifications';
-// import { PUBLIC_URL } from '../../utils/config';
-import axios from 'axios';
+import { PUBLIC_URL } from '../../utils/config';
+// import axios from 'axios';
 
 const Component = (props) => {
   const { script = '', language = 'javascript' } = props;
@@ -15,72 +15,72 @@ const Component = (props) => {
     props.onOutput && props.onOutput(res);
   };
 
-  // const executeCode = async ({ script, language }) => {
-  //   setIsLoading(true);
-  //   props.onOutput && props.onOutput(null);
-
-  //   const worker = new Worker(PUBLIC_URL + '/worker.js');
-
-  //   setTimeout(() => {
-  //     let res = null;
-  //     worker.postMessage(script);
-
-  //     // handle output
-  //     worker.addEventListener('message', function (e) {
-  //       console.log('Message from Worker: ', e.data);
-  //       res = e.data;
-  //       callCallbak(res);
-  //     });
-
-  //     // handle timeouts
-  //     setTimeout(() => {
-  //       worker.terminate();
-  //       if (!res) {
-  //         const timeoutMsg = 'Timeout reached before any output';
-  //         res = {
-  //           date: new Date().toString(),
-  //           hasError: true,
-  //           stdout: [timeoutMsg],
-  //         };
-
-  //         addToast(timeoutMsg, {
-  //           appearance: 'error',
-  //         });
-  //         callCallbak(res);
-  //       }
-  //     }, 5 * 1000);
-  //   }, 500);
-  // };
-
   const executeCode = async ({ script, language }) => {
-    let res = {
-      date: new Date().toString(),
-    };
-    try {
-      console.log('Executing code...', language, script);
-      setIsLoading(true);
-      props.onOutput && props.onOutput(null);
-      const execResponse = await axios.post(
-        `https://cors-anywhere.herokuapp.com/https://sharepad.io/run`,
-        { lang: language, code: script, stdin: '' },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Requested-With': 'XMLHttpRequest',
-          },
+    setIsLoading(true);
+    props.onOutput && props.onOutput(null);
+
+    const worker = new Worker(PUBLIC_URL + '/worker.js');
+
+    setTimeout(() => {
+      let res = null;
+      worker.postMessage(script);
+
+      // handle output
+      worker.addEventListener('message', function (e) {
+        console.log('Message from Worker: ', e.data);
+        res = e.data;
+        callCallbak(res);
+      });
+
+      // handle timeouts
+      setTimeout(() => {
+        worker.terminate();
+        if (!res) {
+          const timeoutMsg = 'Timeout reached before any output';
+          res = {
+            date: new Date().toString(),
+            hasError: true,
+            stdout: [timeoutMsg],
+          };
+
+          addToast(timeoutMsg, {
+            appearance: 'error',
+          });
+          callCallbak(res);
         }
-      );
-
-      res['hasError'] = !execResponse.data.success;
-      res['stdout'] = [execResponse.data.stdout];
-
-      console.log('Executed code...', execResponse);
-    } catch (err) {
-      res['hasError'] = false;
-      res['stdout'] = [err.response.data];
-    }
-    callCallbak(res);
+      }, 5 * 1000);
+    }, 500);
   };
+
+  // const executeCode = async ({ script, language }) => {
+  //   let res = {
+  //     date: new Date().toString(),
+  //   };
+  //   try {
+  //     console.log('Executing code...', language, script);
+  //     setIsLoading(true);
+  //     props.onOutput && props.onOutput(null);
+  //     const execResponse = await axios.post(
+  //       `https://cors-anywhere.herokuapp.com/https://sharepad.io/run`,
+  //       { lang: language, code: script, stdin: '' },
+  //       {
+  //         headers: {
+  //           'Content-Type': 'application/json',
+  //           'X-Requested-With': 'XMLHttpRequest',
+  //         },
+  //       }
+  //     );
+
+  //     res['hasError'] = !execResponse.data.success;
+  //     res['stdout'] = [execResponse.data.stdout];
+
+  //     console.log('Executed code...', execResponse);
+  //   } catch (err) {
+  //     res['hasError'] = false;
+  //     res['stdout'] = [err.response.data];
+  //   }
+  //   callCallbak(res);
+  // };
 
   return (
     <div className='row runner'>
